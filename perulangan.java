c64b713c86d5822280c1491994e7096d9c0bc508
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-13 21:27:34 +0800

    day5
    membuat sebuah program Java yang melakukan perulangan menggunakan loop for. Dengan mencetak angka-angka dari 1 hingga 9 beserta status apakah angka tersebut genap atau ganjil.

---
+++ perulangan.java
@@ -0,0 +1,15 @@
+package day5;
+
+public class perulangan {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i  +"adalah angka genap");
+            }else{
+                System.out.println(i  +"adalah angka ganjil");
+                
+            }
+            
+        }
+    }
+}
