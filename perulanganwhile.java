ecf2c0fbb18921aad4ea7fc654f7f845fd712e44
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-14 22:19:40 +0800

    day6
    membuat program java menggunakan perulangan while untuk mencetak angka 1 sampai 5

---
+++ perulanganwhile.java
@@ -0,0 +1,12 @@
+package day6;
+
+public class perulanganwhile {
+    public static void main(String[] args) {
+        int hitung = 1;
+        while (hitung <= 5){
+            System.out.println("Hitung: " + hitung);
+            hitung++;
+        }
+    }
+    
+}
