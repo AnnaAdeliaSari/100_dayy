db3f3f853139d85c4ab9f11ddab60c51985460ca
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-15 21:22:54 +0800

    day7
    membuat program Java sederhana yang mendefinisikan sebuah array yang berisi daftar nama mahasiswa, kemudian menampilkan isi array pada indeks ke-0 kerena dalam pemrograman java itu nilai terbaca dari 0 jadi yang akan tampil itu "Anna"

---
+++ array.java
@@ -0,0 +1,10 @@
+package day7;
+
+public class array {
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Anna","Fira","Naura","Airin"};
+        
+        System.out.println(mahasiswa[0]);
+    }
+    
+}
