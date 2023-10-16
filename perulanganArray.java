75049ac6d2ae1ba0a544d211233a9bdfd4941ff8
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-16 23:01:01 +0800

    Elemen array menggunakan perulangan
    Membuat program Java yang menggunakan perulangan for untuk mencetak  nama-nama mahasiswa yang disimpan dalam array mahasiswa.

---
+++ perulanganArray.java
@@ -0,0 +1,14 @@
+package day8;
+
+public class perulanganArray {
+    public static void main(String[] args) {
+        String[]mahasiswa = {"Anna","Fira","Naura","Airin"};
+        
+        for (int i = 0; i < mahasiswa.length; i++) {
+            
+            System.out.println(mahasiswa[i]);
+            
+        }
+    }
+    
+}
