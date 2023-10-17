703bbc596448dc468af0d2528aa45f607a62b97a
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-17 23:05:16 +0800

    pertukaran nilai antara dua variabel
    membuat program Java sederhana untuk menukar nilai antara dua variabel. Program ini menggunakan tiga variabel, yaitu a, b, dan c, untuk melakukan pertukaran nilai antara a dan b

---
+++ Swap.java
@@ -0,0 +1,14 @@
+package day9;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 57;
+        int c = a;
+         a = b;
+         b = c;
+         
+         System.out.println(a);
+         System.out.println(b);
+    }
+}
