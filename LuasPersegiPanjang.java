e8c4f067c4c5f0ca52c35b0d89a0ecbcb261b3c7
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-19 23:02:10 +0800

    menghitung luas persegi panjang
    membuat program menghitung luas persegi panjang dengan memberikan nilai inputan panjang dan lebar selama  program dijalankan dan area akan dihitung berdasarkan nilai yang diinput.

---
+++ LuasPersegiPanjang.java
@@ -0,0 +1,18 @@
+package day11;
+
+import java.util.Scanner;
+
+public class LuasPersegiPanjang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     System.out.println("Masukkan panjang persegi panjang:");
+     double panjang = scanner.nextDouble();
+     System.out.println("Masukkan lebar persegi panjang:");
+     double lebar = scanner.nextDouble();
+     
+     //Area = panjang*lebar;
+     double area = panjang*lebar;
+     System.out.println("Luas persegi panjang adalah:"+area);
+ }
+}
+    
