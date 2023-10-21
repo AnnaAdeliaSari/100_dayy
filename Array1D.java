6956a6b95ec38e231a5306756fdc725a409220f7
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-21 23:44:41 +0800

    Array 1 dimensi
     Membuat sebuah array satu dimensi (1D) dengan nama "angka" yang memiliki panjang (length) 5. Kemudian, program ini mengisi nilai-nilai ke dalam array tersebut dan mencetak salah satu nilai dari array ke layar.

---
+++ Array1D.java
@@ -0,0 +1,14 @@
+package day13;
+
+public class Array1D {
+    public static void main(String[] args) {
+        int[] angka = new int[5]; 
+        angka[0] = 5;
+        angka[1] = 10;
+        angka[2] = 20;
+        angka[3] = 25;
+
+        int nilai = angka[3]; 
+        System.out.println(nilai);
+    }
+}
