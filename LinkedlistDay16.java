2ab94bd124a9e858964013e8064d637abd51e185
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-24 22:16:43 +0800

    LinkedList
    Membuat program linkedList "animals" menggunakan metode add() dengan menambahan 3 element  String ("Dog", "Cat", "Cow").Hasil cetak akan menampilkan isi dari LinkedList "animals", yaitu "LinkedList: [Dog, Cat, Cow]".

---
+++ LinkedlistDay16.java
@@ -0,0 +1,17 @@
+package day_100;
+
+import java.util.LinkedList;
+
+public class LinkedlistDay16 {
+  public static void main(String[] args){
+
+    // Membuat linkedlist
+    LinkedList<String> animals = new LinkedList<>();
+
+    // Menambahkan element ke LinkedList
+    animals.add("Dog");
+    animals.add("Cat");
+    animals.add("Cow");
+    System.out.println("LinkedList: "+ animals);
+  }
+}
