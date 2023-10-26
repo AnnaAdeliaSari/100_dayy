eec67fc155919095b26637602d8497ff0dd56f64
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-26 21:23:34 +0800

    Dasar dari logika kondisional if else
    Membuat program Java yang menghitung diskon berdasarkan nilai variabel pembelian, yang mewakili jumlah pembelian. Jika jumlah pembelian lebih besar dari atau sama dengan 100, maka akan diberikan diskon sebesar 20%; jika tidak, maka akan diberikan diskon sebesar 5%.

---
+++ DiskonDay18.java
@@ -0,0 +1,16 @@
+package day_100;
+
+public class DiskonDay18 {
+    public static void main(String[] args) {
+        int pembelian = 200;
+        System.out.println("Jumlah Pembelian = "+ pembelian);
+        
+        if(pembelian >= 100){
+            System.out.println("Diskon 20%");
+        }
+        else{
+            System.out.println("Diskon 5%");
+        }
+    }
+    
+}
