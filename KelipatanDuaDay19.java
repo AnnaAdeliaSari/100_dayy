41d82033f5d2279f8ee3c3d1cd0ccd9a81acf04d
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-10-27 20:37:59 +0800

    Perulangan while mencetak angka kelipatan 2
    Membuat program yang akan mencetak angka-angka kelipatan 2 dari 30 hingga 0 dengan menggunakan perulangan while dengan inisialisasi angka = 30, dan selama angka masih lebih besar atau sama dengan 0, program akan mencetak nilai angka dan kemudian mengurangkan angka sebesar 2 setiap iterasi. Ini akan terus berlanjut hingga mencapai 0.

---
+++ KelipatanDuaDay19.java
@@ -0,0 +1,13 @@
+package day_100;
+
+public class KelipatanDuaDay19 {
+    public static void main(String[] args) {
+        int angka = 30;
+        
+        while (angka >= 0) {
+            System.out.println(angka);
+            angka -= 2;
+        }
+    }
+    
+}
