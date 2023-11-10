746de271bddbd4cdcb4c0cfa848a0b3bd91e6a8f
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-11-10 23:25:55 +0800

    Mengcari Bilangan terbesar
    Program ini meminta untuk memasukkan jumlah bilangan yang akan dimasukkan. Selanjutnya, menggunakan perulangan for untuk mengambil input pengguna sejumlah bilangan yang diinginkan. Saat mengambil setiap bilangan, program memeriksa apakah bilangan tersebut lebih besar dari bilangan terbesar yang telah diinisialisasi sebelumnya.

---
+++ BilanganTerbesarDay34.java
@@ -0,0 +1,33 @@
+package day_100;
+
+import java.util.Scanner;
+
+public class BilanganTerbesarDay34 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah bilangan: ");
+        int jumlahBilangan = scanner.nextInt();
+
+        if (jumlahBilangan <= 0) {
+            System.out.println("Tidak ada bilangan untuk dianalisis.");
+        } else {
+            System.out.print("Masukkan bilangan ke-1: ");
+            int bilanganTerbesar = scanner.nextInt();
+
+            for (int i = 2; i <= jumlahBilangan; i++) {
+                System.out.print("Masukkan bilangan ke-" + i + ": ");
+                int bilangan = scanner.nextInt();
+
+                if (bilangan > bilanganTerbesar) {
+                    bilanganTerbesar = bilangan;
+                }
+            }
+
+            System.out.println("Bilangan terbesar adalah: " + bilanganTerbesar);
+        }
+
+        scanner.close();
+    }
+}
+
