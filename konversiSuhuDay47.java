025365bc394fb58d7180315a7d059bf4dcaaf385
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-11-24 21:48:16 +0800

    konversi suhu dari Celsius ke Fahrenheit
    Membuat program menggunakan kelas Scanner untuk membaca input dari pengguna, dan kemudian kita melakukan konversi suhu dari Celsius ke Fahrenheit dengan rumus yang sesuai. Hasil konversi kemudian ditampilkan kepada pengguna.

---
+++ konversiSuhuDay47.java
@@ -0,0 +1,17 @@
+package day_100;
+
+import java.util.Scanner;
+
+public class konversiSuhuDay47 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan suhu dalam Celsius: ");
+        double suhuCelsius = scanner.nextDouble();
+
+        // Melakukan konversi suhu dari Celsius ke Fahrenheit
+        double suhuFahrenheit = (suhuCelsius * 9/5) + 32;
+
+        System.out.println("Suhu dalam Fahrenheit: " + suhuFahrenheit);
+    }
+}
