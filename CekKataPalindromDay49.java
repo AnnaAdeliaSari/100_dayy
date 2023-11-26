a292a3023557c3e0be80bcbe84e5f80a608fd6ed
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-11-26 16:46:53 +0800

    Mengecek  sebuah kata adalah palindrom atau bukan
    Program ini meminta untuk menginputkan sebuah kata kemudian akan di cek apakah sebuah kata tersebut palindrom atau bukan contoh yang diinputkan adalah "level" dan "java" , mengecek apakah kata tersebut palindrom dengan membuat objek StringBuilder, membalikkan string, dan membandingkan string asli dengan string yang dibalik. kata "level" adalah palindrom dan "java" bukan kata palindrom.

---
+++ CekKataPalindromDay49.java
@@ -0,0 +1,21 @@
+package day_100;
+
+import java.util.Scanner;
+
+public class CekKataPalindromDay49 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah kata: ");
+        String kata = scanner.nextLine();
+
+        // Memanggil metode untuk menentukan apakah kata tersebut palindrom
+        boolean Palindrome = kata.equals(new StringBuilder(kata).reverse().toString());
+
+        if (Palindrome) {
+            System.out.println("Kata \"" + kata + "\" adalah palindrom.");
+        } else {
+            System.out.println("Kata \"" + kata + "\" bukan palindrom.");
+        }
+    }
+}
