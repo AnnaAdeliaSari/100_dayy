0618312bd473f4ecdcd99f37be9f90b52fe86a10
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-12-05 22:12:56 +0800

    Daftar nama menggunakan kelas ArrayList
    Membuat program yang menggunakan kelas ArrayList untuk menyimpan daftar nama. Program kemudian menampilkan judul "Daftar Nama:" ke layar. Selanjutnya, sebuah perulangan for-each digunakan untuk mengakses setiap elemen dalam ArrayList daftarNama. Setiap nama kemudian dicetak ke layar menggunakan System.out.println(nama).

---
+++ Day58.java
@@ -0,0 +1,17 @@
+package day_100;
+
+import java.util.ArrayList;
+
+public class Day58 {
+    public static void main(String[] args) {
+        ArrayList<String> daftarNama = new ArrayList<>();
+        daftarNama.add("Anna");
+        daftarNama.add("Fira");
+        daftarNama.add("Naura");
+
+        System.out.println("Daftar Nama:");
+        for (String nama : daftarNama) {
+            System.out.println(nama);
+        }
+    }
+}
