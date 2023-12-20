58506b9809e0c957a2dfa59a8ae0854ec3a6f6bf
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2023-12-20 23:21:36 +0800

    menampilkan tabel perkalian dari 1 hingga 10
    Membuat program sederhana dengan menggunakan perulangan for , for (int i = 1; i <= 10; i++): perulangan untuk mengontrol iterasi baris dari 1 hingga 10. for (int j = 1; j <= 10; j++):perulangan untuk mengontrol iterasi kolom dari 1 hingga 10. System.out.print(i * j + "\t"): Mencetak hasil perkalian dari nilai i dan j, diikuti dengan karakter tab ("\t"). Ini akan mencetak nilai perkalian tanpa pindah ke baris baru.

---
+++ Day73.java
@@ -0,0 +1,13 @@
+package day_100;
+
+
+public class Day73 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
