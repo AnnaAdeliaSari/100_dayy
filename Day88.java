5449d25cb677999051862999789eae4a8c16fc99
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2024-01-04 22:46:17 +0800

    for-each loop
    Membuat program menggunakan konsep for-each loop untuk mengiterasi melalui elemen-elemen dalam array. Program ini membuat array namaHari yang berisi nama-nama hari dalam seminggu Kemudian Menggunakan for-each loop untuk mengiterasi melalui setiap elemen dalam array namaHari. Variabel hari akan menyimpan nilai dari setiap elemen pada setiap iterasi. Selanjutnya, nilai tersebut dicetak ke layar dengan menggunakan System.out.println(hari).

---
+++ Day88.java
@@ -0,0 +1,14 @@
+package day_100;
+
+
+public class Day88 {
+    public static void main(String[] args) {
+        String[] namaHari = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
+
+        System.out.println("Hari dalam seminggu:");
+
+        for (String hari : namaHari) {
+            System.out.println(hari);
+        }
+    }
+}
