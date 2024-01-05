72763297719742fd9fc68b76a021c1923039e44b
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2024-01-05 21:46:13 +0800

    Array dan loop
    Membuat program  mendeklarasikan array bernama "angka" yang berisi nilai 1, 2, 3, 4, dan 5. Dengan menggunakan perulangan for,  variabel i diinisialisasi dengan nilai 0, dan perulangan akan terus berjalan selama nilai i kurang dari panjang array "angka".

---
+++ Day89.java
@@ -0,0 +1,12 @@
+package day_100;
+
+
+public class Day89 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + angka[i]);
+        }
+    }
+}
