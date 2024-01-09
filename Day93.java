2d54e5ad31682cca5f5b788b535a1b1bd3b6b9c3
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2024-01-09 19:02:15 +0800

    Mencetak nilai menggunakan perulangan  do-while
    Membuat program dengan menggunakan perulangan do-while  dengan mendeklarasikan variabel i  dengan nilai awal 1. Variabel ini akan digunakan sebagai penghitung dalam loop kemudian do {: , ini akan dijalankan setidaknya satu kali, kemudian akan diulang selama kondisi di dalam while benar. i++ berfungsi untuk menambah nilai dari variabel i selama kondisi pada while terpenuhi , contoh  } while (i <= 10); kondisi loop akan mencetak selama nilai i kurang dari atau sama dengan 10.

---
+++ Day93.java
@@ -0,0 +1,14 @@
+package day_100;
+
+
+public class Day93 {
+    public static void main(String[] args) {
+        System.out.println("Hasil perulangan do-while");
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+            
+        } while (i <= 10);
+        }
+    }
