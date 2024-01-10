2d3f34075b278afcd4a7e4de0513afc00524c806
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2024-01-10 21:56:24 +0800

    Peulangan ForEach
    Membuat program dengan menggunakan perulangan foreach , Disini kita membuat suatu array angka yang berisi lima elemen bertipe integer, kemudian memulai perulangan foreach, di mana nilai adalah variabel yang digunakan untuk menyimpan nilai setiap elemen dalam array angka dan kemudian mencetak nilai setiap elemen dalam array ke konsol yaitu 1 2 3 4 5.

---
+++ Day94.java
@@ -0,0 +1,10 @@
+package day_100;
+
+public class Day94 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+        for (int nilai : angka) {
+            System.out.println(nilai);
+        }
+    }
+}
