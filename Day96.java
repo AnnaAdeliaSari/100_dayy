0bfa2bcf20f0af07389814a813cfac0b33026610
AnnaAdeliaSari <147259007+AnnaAdeliaSari@users.noreply.github.com>
2024-01-12 22:04:30 +0800

    Metode karakter
    Membuat codingan Menggunakan metode main untuk memanggil metode karakter, Metode karakter adalah sebuah metode statis yang mencetak karakter dari 'A' hingga 'G' menggunakan perulangan for. Setiap karakter dicetak ke konsol pada baris baru. Jadi, output dari program ini akan mencetak huruf-huruf A, B, C, D, E, F, dan G. dan kemudian mencetaknya ke konsol.

---
+++ Day96.java
@@ -0,0 +1,14 @@
+package day_100;
+
+public class Day96 {
+    public static void main(String[] args) {
+        karakter();
+    }
+
+    public static void karakter() {
+        for (char c = 'A'; c <= 'G'; c++) {
+            System.out.println(c);
+        }
+        
+    }
+}
